79e32976fa4e8c6245d43c8567ea834497e331e5
hirosphere <umoof.x@gmail.com>
2025-06-14 00:24:33 +0900

    25-06-14 A

--- 25/Meh-25-6/ts-src/Q-1/Quest-1-1-DOM.ts
+++ 25/Meh-25-6/ts-src/Q-1/Quest-1-1-DOM.ts
@@ -1,12 +1,5 @@
 import { Leaf , DOM , log } from "../Meh/Meh.js" ;
 
-const time = Leaf ( "" ) ;
-
-setInterval
-(
-	() : void => { time.$ = new Date() .toLocaleTimeString () },
-	1000
-) ;
 
 
 const anm = [ "🐭", "🐄" ,"🐯" , "🐇", "🐉", "🐍", "🐎", "🐏", "🐵", "🐓", "🐶", "🐗" ] ;
@@ -15,8 +8,43 @@ const to_anm = ( s : string ) : string =>
 	s.replace ( /\d/g , m => anm [ + m ] ?? m )
 );
 
+const 辰巳 = () : string =>
+{
+	const daytick = 24 * 3600 * 1000 ;
+	const shift = 9 * 3600 * 1000 ;
+	const dayphase = ( ( new Date().getTime() + shift ) % daytick ) / daytick , m = 12 ;
+	
+	return [
+		
+		r12 ( dayphase , 1 ) ,
+		r12 ( dayphase , 2 ) ,
+		r12 ( dayphase , 3 ) ,
+		r12 ( dayphase , 4 ) ,
+	
+	] .join ( "" ) ;	
+}
+
+const r12 = ( dayphase : number , scale : number ) : string =>
+{
+	const ch = Math.round ( dayphase * 12 ** scale ) % 12 ;
+	return anm [ ch >= 12 ? 0 : ch ] ;
+}
+
+const time = Leaf ( "" ) ;
 const anmtime = time.cvr ( time => to_anm ( time ) ) ;
+const shintime = Leaf ( "" ) ;
+
+const step = () =>
+{
+	time.$ = new Date() .toLocaleTimeString () ;
+	shintime.$ = 辰巳 () ;
+
+	requestAnimationFrame ( step ) ;
+}
+
+requestAnimationFrame ( step ) ;
 
 DOM.add ( time , "#Mehhh" ) ;
-DOM.add ( anmtime , "#Mehhh" ) ;
+// DOM.add ( anmtime , "#Mehhh" ) ;
+DOM.add ( shintime , "#Mehhh" ) ;
 
